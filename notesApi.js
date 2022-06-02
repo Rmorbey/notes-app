23f0aa2566bac339d65c631d99a0f787b562d07c
Russell <russellmorbey@hotmail.co.uk>
2022-06-02 16:19:26 +0100

    NotesApi, createNote and loadNotes methods re-written using try, await, catch error.

--- notesApi.js
+++ notesApi.js
@@ -1,25 +1,52 @@
 class NotesApi {
 
-  loadNotes(callback) {
-    fetch('http://localhost:3000/notes')
-    .then(response => response.json())
-    .then(data => {
-      callback(data);
-    });
+  async loadNotes(callback) {
+    try {
+      const response = await fetch('http://localhost:3000/notes');
+      const notes = await response.json();
+      return callback(notes);
+    } catch(error) {
+      console.log('loadNotes error:', error);
+    }
   }
 
-  createNote(note, callback) {
-    fetch('http://localhost:3000/notes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ "content": note})
-    })
-    .then(response => response.json())
-    .then(data => {
-      callback(data);
-    });
+  // old method that doesn't catch errors
+  // loadNotes(callback) {
+  //   fetch('http://localhost:3000/notes')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     callback(data);
+  //   });
+  // }
+
+  async createNote(note, callback) {
+    try { 
+      const response = await fetch('http://localhost:3000/notes', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ "content": note })
+        });
+      const notes = await response.json();
+      return callback(notes);
+    } catch (error) {
+      console.log('createNote error:', error)
+    }
+
+
+    // old method that doesn't catch errors
+    // fetch('http://localhost:3000/notes', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ "content": note})
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   callback(data);
+    // });
   };
 };
 
